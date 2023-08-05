16e50c859799123daf0ba1b5a4ccdef05c37451e
Bodhi14 <bodhi140802@gmail.com>
2023-08-06 01:03:04 +0530

    Added Vercel Configuration

--- Frontend/src/components/UI/Button/Button.js
+++ Frontend/src/components/UI/Button/Button.js
@@ -1,13 +1,21 @@
 import styles from './Button.module.css';
 import { useDispatch } from 'react-redux';
 import { makeFalseFunc, makeTrueFunc } from '../../../actionTypes/cardAction';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
 export const Button = (props) => {
   const dispatch = useDispatch();
   let answer = "";
   const resumeHandler = () => {
-    answer = window.confirm("If you want a single column resume, press OK else pressing Cancel will lead you to making a mult-column resume");
-
-    if(answer) {
+    answer = window.prompt("If you want a single column resume, type 'y' or 'yes',  else type 'n' or 'no', which will lead you to making a mult-column resume");
+    
+    if(answer=='y' || answer =='yes' || answer=='Yes' || answer == 'YES' || answer == 'Y') {
       dispatch(makeTrueFunc());
     }
     else {
