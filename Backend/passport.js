6bbc57e788b80d1b9a44cb74f3253e3d28a469f4
Bodhi14 <bodhi140802@gmail.com>
2023-08-07 02:36:22 +0530

    Mobile View Integrated

--- Backend/passport.js
+++ Backend/passport.js
@@ -14,11 +14,6 @@ passport.use(
         function (accessToken, refreshToken, profile, callback) {
             console.log(profile);
             email = profile.emails[0].value;
-            
-            // let user = User.findOne({ googleId: profile.id });
-            // if(!user){
-
-            // }
 
             User.findOne({ googleId: profile.id }, (err, user) => {
                 if (!user) {
@@ -33,20 +28,11 @@ passport.use(
     )
 );
 
-/* `passport.serializeUser` is a method provided by the Passport.js library that is used to serialize
-the user object into the session. This method is called after a user is authenticated and its
-purpose is to determine which data of the user object should be stored in the session. In this case,
-the `serializeUser` method is simply passing the entire user object to the `done` callback function,
-which signals that the serialization process is complete. */
+
 passport.serializeUser((user, done) => {
     done(null, user);
 });
 
-/* `passport.deserializeUser` is a method provided by the Passport.js library that is used to
-deserialize the user object from the session. This method is called every time a request is made to
-the server and its purpose is to retrieve the user object from the session and make it available in
-the request object. In this case, the `deserializeUser` method is simply passing the entire user
-object to the `done` callback function, which signals that the deserialization process is complete. */
 passport.deserializeUser((user, done) => {
     done(null, user);
 });
