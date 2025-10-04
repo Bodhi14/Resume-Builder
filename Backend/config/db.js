f21693cb00206f5d403532f8e4bf416f753f549c
techmaniac02 <117096012+techmaniac02@users.noreply.github.com>
2025-10-04 18:23:31 +0000

    Fixing the backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,17 +2,23 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDb = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+  try {
+    const uri = process.env.MONGO_URI;
 
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-        process.exit()
-    }
-}
+    const clientOptions = {
+      serverApi: {
+        version: '1',
+        strict: true,
+        deprecationErrors: true,
+      },
+    };
+
+    const conn = await mongoose.connect(uri, clientOptions);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDb;
